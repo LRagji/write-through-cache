478b493c3d5676dfd29f0e73002cbf8bc7e84a25
LaukikOnOMac <22937426+LRagji@users.noreply.github.com>
2021-10-17 22:37:40 +0530

    moved Identity conversion outside of the library

--- source/i-dimensional-data.ts
+++ source/i-dimensional-data.ts
@@ -1,4 +1,4 @@
 export interface IDimensionalData {
     payload: string;
-    dimensions: Array<string>;
+    dimensions: Array<bigint>;
 }
--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,5 @@
 import { IBulkResponse } from "./i-bulk-response";
 import { IDimensionalData } from "./i-dimensional-data";
-import { DimensionalIdentity } from "./identity/dimensional-identity";
-import { IMonotonicIdentityResolver } from "./identity/i-monotonic-identity-resolver";
 import { IError } from "./i-error";
 import { LogStructureMeta } from "./log-structure/log-structure-meta";
 import { IPartitionResolver } from "./partitions/i-partition-resolver";
@@ -10,31 +8,38 @@ import { IShardResolver } from "./shard/i-shard-resolver";
 import { Shard } from "./shard/shard";
 
 export class WriteThroughCache {
-    private dimensionalIdentity: DimensionalIdentity;
     private partitionBuilder: PartitionBuilder;
     private logStructureMeta: LogStructureMeta;
     private shardResolvers: Shard;
 
-    constructor(identityResolvers: Array<IMonotonicIdentityResolver>, partitionResolvers: Array<IPartitionResolver>, shardResolvere: Array<IShardResolver>, seperatorChar = "*") {
-        if ((identityResolvers.length !== partitionResolvers.length &&
-            shardResolvere.length !== partitionResolvers.length) ||
-            partitionResolvers.length === 0) {
-            throw new Error(`Configuration mismatch "identityResolvers"(${identityResolvers.length}), "partitionResolvers"(${partitionResolvers.length}), "shardResolvere"(${shardResolvere.length}) should match and not be 0.`);
+    constructor(partitionResolvers: Array<IPartitionResolver>, shardResolvere: Array<IShardResolver>, seperatorChar = "*") {
+        if ((shardResolvere.length !== partitionResolvers.length) || partitionResolvers.length === 0) {
+            throw new Error(`Configuration mismatch "partitionResolvers"(${partitionResolvers.length}), "shardResolvere"(${shardResolvere.length}) should match and not be 0.`);
         }
-        this.dimensionalIdentity = new DimensionalIdentity(identityResolvers)
         this.partitionBuilder = new PartitionBuilder(partitionResolvers, seperatorChar);
         this.logStructureMeta = new LogStructureMeta();
         this.shardResolvers = new Shard(shardResolvere);
     }
 
     async write(rawData: Array<IDimensionalData>): Promise<IBulkResponse<Array<IDimensionalData>, Array<IError<IDimensionalData>>>> {
-        const identityResponse = await this.dimensionalIdentity.identify(rawData);
-        const lsmResponse = await this.logStructureMeta.appendMeta(identityResponse.succeeded);
+        const lsmResponse = await this.logStructureMeta.appendMeta(rawData);
         const partitionResponse = await this.partitionBuilder.build2(lsmResponse.succeeded);
         const shardResponse = await this.shardResolvers.write(partitionResponse.succeeded);
         return {
             succeeded: shardResponse.succeeded,
-            failed: Array(...identityResponse.failed, ...lsmResponse.failed, ...partitionResponse.failed, ...shardResponse.failed)
+            failed: Array(...lsmResponse.failed, ...partitionResponse.failed, ...shardResponse.failed)
         };
+    }
+
+    async read(): Promise<Array<IDimensionalData>> {
+        throw new Error("Not Implemented");
+    }
+
+    async purgeAcquire(): Promise<Array<IDimensionalData>> {
+        throw new Error("Not Implemented");
+    }
+
+    async purgeRelease(): Promise<Array<IDimensionalData>> {
+        throw new Error("Not Implemented");
     }
 }
